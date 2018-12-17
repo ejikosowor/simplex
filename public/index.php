0abe33c2bf113b80ec4565e801d2e75327bbcded
Dixon Etta-Ekuri <jikjones42@gmail.com>
2018-12-18 01:42:59 +0300

    Simplex V2

--- public/index.php
+++ public/index.php
@@ -4,15 +4,10 @@ chdir(dirname(__DIR__));
 
 require_once 'vendor/autoload.php';
 
-use App\Simplex\Framework;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\Reference;
 
-$request = Request::createFromGlobals();
-
 $container = include 'app/container.php';
-$config = include 'app/config.php';
-$database = include 'app/database.php';
+$request = $container->get('request');
 
 $response = $container->get('framework')->handle($request);
 
