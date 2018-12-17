c4e0b84f069ba31ad1e1a8bd5f610189093e8f32
Dixon Etta-Ekuri <jikjones42@gmail.com>
2018-12-18 01:46:04 +0300

    Simplex V2 Cleanup

--- cli-config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// replace with file to your own project bootstrap
-require_once __DIR__.'/public/index.php';
-
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
